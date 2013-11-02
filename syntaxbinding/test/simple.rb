0d2d022be8344d78befd831e9e708a2b0212c070
Veit Jahns <nuncupatio@gmail.com>
2013-11-02 12:54:37 +0100

    Nested reused property

--- syntaxbinding/test/simple.rb
+++ syntaxbinding/test/simple.rb
@@ -74,6 +74,20 @@ class Property < Test::Unit::TestCase
 		assert_equal tokens[5], "/ROOT/ELEMENT_CONCEPT_2/ELEMENT_PROPERTY"
 	end
 	
+	def assert_id_nested(result)
+		tokens = result.split(/[;\n]/)
+
+		assert_equal result.lines.count, 2
+		
+		assert_equal tokens[0], "bii:tir19-001-Id"
+		assert_equal tokens[1], "http://spec.cenbii.eu/BII2#tir19-Class"
+		assert_equal tokens[2], "/ROOT/ELEMENT_CONCEPT_1[ELEMENT_PREDICATE='a']/ELEMENT_CONCEPT_2/ELEMENT_PROPERTY"
+		
+		assert_equal tokens[3], "bii:tir19-004-IdB"
+		assert_equal tokens[4], "http://spec.cenbii.eu/BII2#tir19-ClassB"
+		assert_equal tokens[5], "/ROOT/ELEMENT_CONCEPT_1[ELEMENT_PREDICATE='b']/ELEMENT_CONCEPT_2/ELEMENT_PROPERTY"
+	end
+	
 	def test_property_reference
 		out = do_xslt("data/property_reference.xsd")
 		assert_id_only(out)
@@ -107,5 +121,10 @@ class Property < Test::Unit::TestCase
 	def test_property_type_reuse_noreference
 		out = do_xslt("data/property_type_reuse_noreference.xsd")
 		assert_id_two(out);
+	end
+	
+	def test_property_reference_reuse_nested
+		out = do_xslt("data/property_reference_reuse_nested.xsd")
+		assert_id_nested(out);
 	end
 end
