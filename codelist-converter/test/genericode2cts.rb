e428ff67e866387c67fe1bbb3e2de025f361cd19
Veit Jahns <nuncupatio@gmail.com>
2014-01-12 15:53:42 +0100

    Updated copyright notices

--- codelist-converter/test/genericode2cts.rb
+++ codelist-converter/test/genericode2cts.rb
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 #
-# Copyright (c) 2013 Veit Jahns
+# Copyright (c) 2013-2014 Veit Jahns
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
